010da6bf5e7f50e7c274b18c3909bd863b2ac469
Robert te Kaat <robert.te.kaat@gmail.com>
2017-01-27 10:15:29 +0100

    build.sh completed

--- 0.7.0/build.sh
+++ 0.7.0/build.sh
@@ -1,3 +1,5 @@
 rm -r --force ./app/
 git clone -q --branch=master https://github.com/kwaazaar/resonance.web.git ./app/
-docker run -it -v ./app --workdir /app microsoft/aspnetcore-build bash -c "dotnet restore && dotnet publish -c Release -o ./bin/Release/PublishOutput"
+docker run -it -v $(pwd)/app:/app --workdir /app/Resonance.Web microsoft/aspnetcore-build bash -c "dotnet restore && dotnet publish -c Release -o ./bin/Release/PublishOutput"
+mkdir release
+cp app/Resonance.Web/bin/Release/PublishOutput?/* ./release -r
