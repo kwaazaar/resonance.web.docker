fb1f44033f1ad8e4322459384452f7b06cb01b49
Robert te Kaat <robert.te.kaat@gmail.com>
2017-01-28 17:42:09 +0100

    Works

--- 0.7.0/build.sh
+++ 0.7.0/build.sh
@@ -1,5 +1,14 @@
-rm -r --force ./app/
+#!/bin/bash
+
+# Remove exising app folder (leftover)
+rm -rf ./app/
+
+# Download sources from GitHub
 git clone -q --branch=master https://github.com/kwaazaar/resonance.web.git ./app/
-docker run -it -v $(pwd)/app:/app --workdir /app/Resonance.Web microsoft/aspnetcore-build bash -c "dotnet restore && dotnet publish -c Release -o ./bin/Release/PublishOutput"
-mkdir release
-cp app/Resonance.Web/bin/Release/PublishOutput?/* ./release -r
+
+# Use aspnetcore-build docker-image to compile sources and copy the results to the /app/publish folder
+docker run -it --rm -v $(pwd)/app:/app --workdir /app microsoft/aspnetcore-build bash -c "dotnet restore && dotnet publish Resonance.Web/project.json -c Release -o /app/publish"
+
+# Create a docker image based on the compile results (remove existing first). Check Dockerfile for its instructions.
+docker rmi kwaazaar/resonance.web
+docker build -t kwaazaar/resonance.web .
---
+++ 0.7.0/run.sh
@@ -0,0 +1,1 @@
+docker run --rm -d -p 8000:80 kwaazaar/resonance.web
