a501daaace07ed47c74eb32fc2fa3e33e530613b
Robert te Kaat <robert.te.kaat@gmail.com>
2017-02-02 13:33:33 +0100

    Clean up

--- 0.7.0/build-mysql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Download DB-script for MySql
-mkdir resonancedb
-wget https://raw.githubusercontent.com/kwaazaar/Resonance/master/Resonance.Core/content/ResonanceDB.MySql.sql -O resonancedb/ResonanceDB.MySql.sql
-
