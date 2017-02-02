fda3dfe35371c4f37b383c09ac93562075a2115e
Robert te Kaat <robert.te.kaat@gmail.com>
2017-02-02 13:32:52 +0100

    Moved to folder latest

---
+++ latest/build-image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Remove exising app folder (leftover)
+rm -rf ./app/
+
+# Download sources from GitHub
+git clone -q --branch=master https://github.com/kwaazaar/resonance.web.git ./app/
+
+# Use aspnetcore-build docker-image to compile sources and copy the results to the /app/publish folder
+docker run -it --rm -v $(pwd)/app:/app --workdir /app microsoft/aspnetcore-build bash -c "dotnet restore && dotnet publish Resonance.Web/project.json -c Release -o /app/publish"
+
+# Create a docker image based on the compile results (remove existing first). Check Dockerfile for its instructions.
+docker rmi kwaazaar/resonance:latest
+docker build . -t kwaazaar/resonance:latest
---
+++ latest/get-resonancedb-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Download DB-script for MySql
+mkdir resonancedb
+wget https://raw.githubusercontent.com/kwaazaar/Resonance/master/Resonance.Core/content/ResonanceDB.MySql.sql -O resonancedb/ResonanceDB.MySql.sql
+
