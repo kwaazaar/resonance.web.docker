d1eaaea8f813b413991a7f708b8e63935ea5eb29
kwaazaar <robert.te.kaat@gmail.com>
2017-02-10 15:02:13 +0100

    build-image renamed

--- latest/build-image.ps1
+++ latest/build-image.ps1
@@ -1,5 +1,4 @@
-#!/bin/bash
-
 # Create a docker image based on the compile results (remove existing first). Check Dockerfile for its instructions.
 docker rmi kwaazaar/resonance:latest
 docker build -f Dockerfile.win -t kwaazaar/resonance:latest .
+././
