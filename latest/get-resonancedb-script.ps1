8091264a1ebfc10821574bc0f09f6252b6d54d9f
kwaazaar <robert.te.kaat@gmail.com>
2017-02-17 15:01:32 +0100

    yaml changed, download of mdf/ldf for sqlexpress

--- latest/get-resonancedb-script.ps1
+++ latest/get-resonancedb-script.ps1
@@ -1,4 +1,5 @@
 # Download DB-script for MySql
 New-Item -ItemType Directory resonancedb -ErrorAction Ignore
-wget https://raw.githubusercontent.com/kwaazaar/Resonance/master/Resonance.Core/content/ResonanceDB.MySql.sql -O resonancedb/ResonanceDB.MySql.sql
+wget https://raw.githubusercontent.com/kwaazaar/Resonance/master/Resonance.Core/content/ResonanceDB.mdf -O resonancedb/ResonanceDB.mdf
+wget https://raw.githubusercontent.com/kwaazaar/Resonance/master/Resonance.Core/content/ResonanceDB_log.ldf -O resonancedb/ResonanceDB_log.ldf
 
